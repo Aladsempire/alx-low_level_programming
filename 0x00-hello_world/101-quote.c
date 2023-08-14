bcab3dd4f509eabb815e50cfcfda67725f5a3e3b
Aladsempire <adaorakamelu@gmail.com>
2023-08-14 04:57:28 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - A c program that prints a line to the standard error
  *
- * Return 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
